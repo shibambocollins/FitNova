721a5d8e16d48a6184bb1092914b00566732d757
IngaPlati <230126634@mycput.ac.za>
2026-03-15 23:43:54 +0200

    Updated GeoLocation to use Builder Pattern

--- src/main/java/za/ac/cput/domain/GeoLocation.java
+++ src/main/java/za/ac/cput/domain/GeoLocation.java
@@ -2,22 +2,39 @@ package za.ac.cput.domain;
 
 public class GeoLocation {
 
-    private double latitude;
-    private double longitude;
+        private double latitude;
+        private double longitude;
 
-    public GeoLocation (double latitude, double longitude){
-        this.latitude = latitude;
-        this.longitude = longitude;
-    }
+        private GeoLocation(Builder builder) {
+            this.latitude = builder.latitude;
+            this.longitude = builder.longitude;
+        }
 
-    public double getLatitude() {
-        return latitude;
-    }
+        public double getLatitude() {
+            return latitude;
+        }
 
-    public double getLongitude() {
-        return longitude;
+        public double getLongitude() {
+            return longitude;
+        }
+
+        public static class Builder {
+
+            private double latitude;
+            private double longitude;
+
+            public Builder setLatitude(double latitude) {
+                this.latitude = latitude;
+                return this;
+            }
+
+            public Builder setLongitude(double longitude) {
+                this.longitude = longitude;
+                return this;
+            }
+
+            public GeoLocation build() {
+                return new GeoLocation(this);
+            }
+        }
     }
-    public String getLocation(){
-        return "Latitude: " + latitude + ", Longitude: " + longitude;
-    }
-}
