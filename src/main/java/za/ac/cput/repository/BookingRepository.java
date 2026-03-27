2ae19a937cbace3da596877a7a37cd80889d6152
shibambocollins <ntsobokwanec@gmail.com>
2026-03-27 11:02:54 +0200

    Update BookingRepository interface

--- src/main/java/za/ac/cput/repository/BookingRepository.java
+++ src/main/java/za/ac/cput/repository/BookingRepository.java
@@ -1,8 +1,11 @@
 package za.ac.cput.repository;
 
+import za.ac.cput.entity.Booking;
+
 /* Booking.java
 Author: Collins Shibambo (230093183)
 Date: 25 March 2026
 */
-public interface BookingRepository {
+public interface BookingRepository extends IRepository<Booking, String> {
+
 }
